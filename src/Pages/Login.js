6ed2c3374307a438fda3e9f85df958d4cb15bdc8
jpr34 <jpr34@uwf.students.edu>
2023-10-19 17:28:53 -0500

    inital react setup

---
+++ src/Pages/Login.js
@@ -0,0 +1,17 @@
+
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="loginPage">
+      <header>
+        Login Page
+      </header>
+      <button>
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;
