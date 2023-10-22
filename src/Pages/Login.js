ba2b7a0f4e24c5d710d9cdcddcc7b28deb75fad8
itchygenji <itchygenji@gmail.com>
2023-10-22 12:43:01 -0400

    Added Hello world, login page leads to it

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,13 +1,22 @@
-
+import React from 'react';
 import './Login.css';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  let navigate = useNavigate();
+
+  const handleLogin = () => {
+    // Here, you'd typically validate the user's login details.
+    // For this example, we'll assume login is always successful.
+    navigate('/hello-world');
+  };
+
   return (
     <div className="loginPage">
       <header>
         Login Page
       </header>
-      <button>
+      <button onClick={handleLogin}>
         Login
       </button>
     </div>
