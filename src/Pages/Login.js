d34685b628953b91fa8a9886f1cbafd9cc6fabeb
itchygenji <itchygenji@gmail.com>
2023-10-22 11:52:14 -0400

    Deleted a bunch of stuff

---
+++ src/Pages/Login.js
@@ -0,0 +1,16 @@
+import './Login.css';
+
+function Login() {
+  return (
+    <div className="loginPage">
+      <header>
+        Login Page
+      </header>
+      <button>
+        Login
+      </button>
+    </div>
+  );
+}
+
+export default Login;
