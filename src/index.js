79639cff68f31cbb1604a3cccc14affd34e06f63
itchygenji <itchygenji@gmail.com>
2023-10-22 13:50:51 -0400

    Had to add a default path

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/hello-world" element={<HelloWorld />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </Router>
   </React.StrictMode>
