0d4e147566ccfc3e95d1243bc1a694b832a77f35
itchygenji <itchygenji@gmail.com>
2023-10-31 19:34:15 -0400

    Added place holder for tasks

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
 function Home() {
+  const [tasks, setTasks] = useState([]); // Initial empty array of tasks
   const location = useLocation();
   const navigate = useNavigate();
 
@@ -23,11 +24,34 @@ function Home() {
       });
   };
 
+  const addTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+    // Here you would also send a request to add the task to the backend
+  };
+
+  const removeTask = (taskIndex) => {
+    const updatedTasks = tasks.filter((_, index) => index !== taskIndex);
+    setTasks(updatedTasks);
+    // Similarly, send a request to remove the task from the backend
+  };
+
   return (
     <div>
       <h1>Hello, {location.state.user.given_name}</h1>
       <p>Welcome to the application!</p>
       <button onClick={handleGoToProfile}>View Profile</button>
+
+      <div>
+        <h2>Your Tasks</h2>
+        <ul>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              {task} <button onClick={() => removeTask(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => addTask('New Task')}>Add Task</button>
+      </div>
     </div>
   );
 }
