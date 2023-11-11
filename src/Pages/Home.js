0d52e2053eb0b88f38cfa6eefd839ad3b5b0ae8d
itchygenji <itchygenji@gmail.com>
2023-11-10 20:52:54 -0500

    Added logout button and return home button

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,26 +1,23 @@
-
 import React, { useState, useEffect } from 'react';
-import '../css/Home.css'
+import '../css/Home.css';
 import { useLocation, useNavigate } from 'react-router-dom';
+import { googleLogout } from '@react-oauth/google'; 
 import AddTaskForm from '../Components/AddTaskForm';
 
 function Home(props) {
-  const [tasks, setTasks] = useState([]); // Initial empty array of tasks
-  const [showTaskForm, setShowTaskForm] = useState(false)
-  const [title, setTitle] = useState("")
-  const [description, setDescription] = useState("")
-  const [dueDate, setDueDate] = useState("")
-  const [priority, setPriority] = useState("")
-  const [category, setCategory] = useState("")
-  
-  
+  const [tasks, setTasks] = useState([]);
+  const [showTaskForm, setShowTaskForm] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [priority, setPriority] = useState("");
+  const [category, setCategory] = useState("");
+
   const location = useLocation();
   const navigate = useNavigate();
   const userEmail = location.state.user.email;
 
-
   const handleGoToProfile = () => {
-
     fetch(`http://localhost:8080/findUserByEmail/${userEmail}`)
       .then((response) => {
         if (response.ok) {
@@ -37,7 +34,6 @@ function Home(props) {
   };
 
   const confirmTask = () => {
-
     let taskData = {
       title: title,
       description: description,
@@ -45,10 +41,9 @@ function Home(props) {
       priority: priority,
       category: category,
       userId: userEmail
-    }    
-    console.log("new task created with taskData: ", taskData)
+    }
+    console.log("new task created with taskData: ", taskData);
 
-    // send the data to the backend 
     fetch(`http://localhost:8080/addTask`, {
         method: 'POST',
         body: JSON.stringify(taskData),
@@ -57,66 +52,73 @@ function Home(props) {
         }
     })
     .then(res => {
-      res.json()
-      console.log("task sent to backend with taskData ", taskData)
+      res.json();
+      console.log("task sent to backend with taskData ", taskData);
 
-      setTasks([...tasks, taskData])
+      setTasks([...tasks, taskData]);
 
-      setShowTaskForm(false)
-      setTitle("")
-      setDescription("")
-      setDueDate("")
-      setPriority("")
-      setCategory("")
+      setShowTaskForm(false);
+      setTitle("");
+      setDescription("");
+      setDueDate("");
+      setPriority("");
+      setCategory("");
     })
-  }
+  };
+
   const addTask = (newTask) => {
-    setShowTaskForm(true) 
-    }
+    setShowTaskForm(true);
+  };
 
   const removeTask = (taskIndex) => {
     const updatedTasks = tasks.filter((_, index) => index !== taskIndex);
     setTasks(updatedTasks);
-    // Similarly, send a request to remove the task from the backend
+  };
+
+  
+  const handleLogout = () => {
+    googleLogout(); 
+    navigate('/login');
   };
 
   return (
     <div className='home'>
       <h1>Hello, {location.state.user.given_name}</h1>
       <p>Welcome to the application!</p>
+
+      <button onClick={handleLogout}>Logout</button>
       <button onClick={handleGoToProfile}>View Profile</button>
 
       <div className="tasks-container">
         <h2>Your Tasks</h2>
         <ul>
-            {Array.isArray(tasks) && tasks.map((task, index) => (
-              <li key={index}>
-                <div className="task">
-                  <h3>{task.title}</h3>
-                  <p>{task.description}</p>
-                  <p>Due date: {task.dueDate}</p>
-                  <p>Priority: {task.priority}</p>
-                  <p>Category: {task.category}</p>
-                </div>
-                <button onClick={() => removeTask(index)}>Remove</button>
-              </li>
-            ))}
+          {Array.isArray(tasks) && tasks.map((task, index) => (
+            <li key={index}>
+              <div className="task">
+                <h3>{task.title}</h3>
+                <p>{task.description}</p>
+                <p>Due date: {task.dueDate}</p>
+                <p>Priority: {task.priority}</p>
+                <p>Category: {task.category}</p>
+              </div>
+              <button onClick={() => removeTask(index)}>Remove</button>
+            </li>
+          ))}
         </ul>
         <button onClick={() => addTask('New Task')}>Add Task</button>
         
         { showTaskForm &&    
           <div>  
-          <AddTaskForm 
-            title={title} setTitle={setTitle}
-            description={description} setDescription={setDescription}
-            dueDate={dueDate} setDueDate={setDueDate}
-            priority={priority} setPriority={setPriority}
-            category={category} setCategory={setCategory}
+            <AddTaskForm 
+              title={title} setTitle={setTitle}
+              description={description} setDescription={setDescription}
+              dueDate={dueDate} setDueDate={setDueDate}
+              priority={priority} setPriority={setPriority}
+              category={category} setCategory={setCategory}
             />
-          <button onClick={() => confirmTask()}>Create</button>
+            <button onClick={() => confirmTask()}>Create</button>
           </div>
         }
-        
       </div>
     </div>
   );
